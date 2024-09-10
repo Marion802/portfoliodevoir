0da57263f0a459eb6f9dccf4defebb7315b557d0
marion.sermande <marion.sermande@gmail.com>
2024-09-10 06:07:37 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <formulaire></formulaire>
+
+  <footer id="pied" class="roseclair">
+      <a href="https://github.com/Marion802"><img src="/src/assets/logo-github.png" alt="logo-github"></a>
+      <a href="https://www.linkedin.com/in/marion-sermande-1a3208307/"><img src="/src/assets/linkedin.png" alt="logo-linkedin"></a>
+      <a href="https://x.com/MarionSermande"><img src="/src/assets/twitter.png" alt="logo-twitter"></a>
+      <small>Mise à jour le <time datetime="2024-18-09"> 18 septembre 2024</time></small>
+    </footer>
 
 </template>
 
@@ -6,6 +14,11 @@
 
 
 <script setup>
+
+import formulaire from './components/HelloWorld.vue'
+
+const form = document.getElementById (formulaire);
+
 
 </script>
 
@@ -15,4 +28,12 @@
 
 <style scoped>
 
+.roseclair {
+  background-color: #ffa9f9;
+}
+
+#pied img {
+  width: 50px;
+  height: 50px;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,77 @@
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100..400&display=swap" rel="stylesheet">
+
+<body class="fond">
+      <header id="premierepartie" >
+        <h1 class="uniquifier"> Marion SERMANDE </h1>
+        <h2>Mon portfolio</h2>
+        <!--mettre le  lien logo de l'entreprise -->
+          <a href=""><img src="/src/assets/logo.cocoweb.png" alt="logo-entreprise-cocoweb" id="cocoweb"></a>
+        <nav>
+          <ul>
+            <li> <a href="">Présentaion</a></li>
+            <li> <a href="">Création</a></li>
+            <li> <a href="">Formutaire de contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      
+      <main>
+      
+        <div id="present">
+        <p>
+          Je me présente Marion SERMANDE, heureuse fondatrice de COCO WEB. <br/>
+          Specialiste du developpeur web, COCO WEB à été créer pour donner une image et de la visibilité a vos idées. <br/>
+          Je vous laisse jeter un coup d'oeil sur les compétences acquises de COCO. 
+        </p>
+      </div>
+
+      <div id="cv">
+        <h3>CV en html</h3>
+        <a href=""><img src="" alt=""></a>
+      </div>
+
+      <div id="cahierdescharges">
+        <h3>Cahier des charges</h3>
+        <a href=""><img src="" alt=""></a>
+      </div>
+
+      <div id="espacecomment">
+        <h3>Dynamiser un espace commentaire</h3>
+        <a href=""><img src="" alt=""></a>
+      </div>
+
+
+      <form action="#" method="post" id="formulaire">
+        <h2>Contactez Coco</h2>
+        <div>
+          <label for="first-name">Prénom</label>
+          <input type="text" name="first-name" id="first-name" value="first-name">
+        </div>
+        
+        <div>
+          <label for="last-name" >Nom</label>
+            <input type="text" name="last-name" id="last-name">
+        </div>
+        
+        <div>
+          <label for="object" >Objet</label>
+            <input type="text" name="object" id="object">
+        </div>
+        
+        <div>
+          <label for="message" >Commentaire</label>
+          <textarea id="message" name="message" rows="4"  ></textarea>
+        
+        </div>
+        <button type="submit"> Envoyer </button>
+      </form>
+    </main>
+
+</body>
+
 
 </template>
 
@@ -6,7 +79,7 @@
 
 
 <script setup>
-
+import pied from '@/App.vue'
 </script>
 
 
@@ -14,4 +87,23 @@
 
 <style scoped>
 
+.uniquifier {
+  font-family: "Playwrite CU";
+  font-optical-sizing: auto;
+  font-weight: 100;
+  font-style: normal;
+}
+
+
+
+.fond {
+  background-image: url(/src/assets/fond.png);
+}
+
+
+
+#cocoweb {
+  width:150px ;
+  height: 150px;
+}
 </style>
